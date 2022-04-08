1615bf4b62d6eaf88aaff9a338ca3d8037a5c3d2
vinaykumar7081 <vinaykumarvinay165@gmail.com>
2022-04-08 14:15:39 +0530

    [VINAY] Add: you can roll again die, if you want to next number

--- SnakeLadderGame/SnakeGame.cs
+++ SnakeLadderGame/SnakeGame.cs
@@ -8,12 +8,29 @@ namespace SnakeLadderGame
 {
     public class SnakeGame
     {
-        const int position = 0;
+        const int NO_PLAY = 0, SNAKE = 1, LADDER = 2;
+        int position = 0;
         public int dieRoll()
+        {
+            Random random = new Random();
+            int diePosition = random.Next(1,7);
+            return diePosition;
+        }
+        public void gamePlaying()
         { 
             Random random = new Random();
-            int roll = random.Next(1, 7);
-          return roll;
+            int roll = random.Next(0, 3);
+
+            switch (roll)
+            {
+                case NO_PLAY:this.position += 0;
+                    break;
+                case SNAKE:this.position-=this.dieRoll();
+                    break;
+                case LADDER:this.position+=this.dieRoll();
+                    break;
+            }
+          
         }
         
     }
