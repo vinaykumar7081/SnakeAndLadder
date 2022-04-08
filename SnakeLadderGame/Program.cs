e910f09cba8de9f79865aef6f3082c7c01475b73
vinaykumar7081 <vinaykumarvinay165@gmail.com>
2022-04-08 14:39:54 +0530

    [VINAY] Add: Congratulations, you have got maximum nuber

--- SnakeLadderGame/Program.cs
+++ SnakeLadderGame/Program.cs
@@ -5,7 +5,7 @@ public class program
     public static void Main(String[] args)
     { 
     SnakeGame game = new SnakeGame();
-       int start= game.dieRoll();
-        Console.WriteLine("Welcome in the Game, You got : "+ start+ "  " +"Position" );
+
+        game.gamePlaying();
     }
 }
--- SnakeLadderGame/SnakeGame.cs
+++ SnakeLadderGame/SnakeGame.cs
@@ -17,20 +17,32 @@ namespace SnakeLadderGame
             return diePosition;
         }
         public void gamePlaying()
-        { 
-            Random random = new Random();
-            int roll = random.Next(0, 3);
-
-            switch (roll)
+        {
+            while (this.position < 100)
             {
-                case NO_PLAY:this.position += 0;
-                    break;
-                case SNAKE:this.position-=this.dieRoll();
-                    break;
-                case LADDER:this.position+=this.dieRoll();
-                    break;
+                Random random = new Random();
+                int roll = random.Next(0, 3);
+
+                switch (roll)
+                {
+                    case NO_PLAY:
+                        this.position += 0;
+                        break;
+                    case SNAKE:
+                        this.position -= this.dieRoll();
+                        if (this.position < 0)
+                        {
+                            this.position = 0;
+                        }
+                        break;
+                    case LADDER:
+                        this.position += this.dieRoll();
+                        break;
+                }
+
             }
-          
+            Console.WriteLine("You Have got "+this.position+ " "+" Number");
+
         }
         
     }
