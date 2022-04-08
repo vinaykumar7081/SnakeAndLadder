3a531e86e8b97ff236cbb4eb2ba9f07e3c208c3a
vinaykumar7081 <vinaykumarvinay165@gmail.com>
2022-04-08 13:03:03 +0530

    [VINAY] Add: Welcome in thi Game

---
+++ SnakeLadderGame/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using SnakeLadderGame;
+public class program
+{
+    public static void Main(String[] args)
+    { 
+    SnakeGame game = new SnakeGame();
+       int start= game.dieRoll();
+        Console.WriteLine("Welcome in the Game, Your Position is: " + "****"+ start+"****");
+    }
+}
