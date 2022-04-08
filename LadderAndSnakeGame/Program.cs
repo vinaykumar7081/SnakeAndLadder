eda0b5b4ce797ca3e7c470dbc95b0c7a6788c8b7
vinaykumar7081 <vinaykumarvinay165@gmail.com>
2022-04-09 00:31:17 +0530

    [INAY] Add: Display the winner Player Name

---
+++ LadderAndSnakeGame/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using LadderAndSnakeGame;
+public class program
+{
+    public static void Main(String[] args)
+    {
+        SnakeGame game = new SnakeGame();
+
+        game.playingGame();
+    }
+}
---
+++ LadderAndSnakeGame/SnakeGame.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LadderAndSnakeGame
+{
+    internal class SnakeGame
+    {
+        const int NO_PLAY = 0, SNAKE = 1, LADDER = 2;
+        int roll1 = 0, roll2 = 0, position = 0;
+
+        public int dieRoll()
+        {
+            Random random = new Random();
+            int diePosition = random.Next(1, 7);
+            return diePosition;
+        }
+        public void playingGame()
+        {
+
+            while (this.position < 100)
+            {
+
+                Random random1 = new Random();
+                int player1 = random1.Next(0, 3);
+
+                Random random2 = new Random();
+                int player2 = random2.Next(0, 3);
+
+                if (player1.Equals(LADDER))
+                {
+                    this.position += this.dieRoll();
+                    roll1 += this.position;
+                    continue;
+                }
+                else if (player2.Equals(LADDER))
+                {
+                    this.position += this.dieRoll();
+                    roll1 += this.position;
+                    continue;
+                }
+                else if (player1.Equals(SNAKE))
+                {
+                    this.position -= this.dieRoll();
+                    roll1 += this.position;
+                }
+                else if (player2.Equals(SNAKE))
+                {
+                    this.position -= this.dieRoll();
+                    roll2 += this.position;
+                }
+            }
+            if (roll1 > roll2)
+            {
+                Console.WriteLine("Player First is Winner with" + " " + roll1);
+            }
+            else if (roll2 > roll1)
+            {
+                Console.WriteLine("Player Second is Winner with" + " " + roll2);
+            }
+            else
+            {
+                Console.WriteLine("Game is tie");
+            }
+            Console.WriteLine("First Player getting number is " + " " + roll1);
+            Console.WriteLine("Second Player getting number is " + " " + roll2);
+
+        }
+    }
+}
